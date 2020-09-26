14529ed108ea8a3b412f86716d858cd282d39ab3
Ade Adeola <adedotster@gmail.com>
2020-09-26 12:44:48 +0200

    dimension created

---
+++ theme/dimension.ts
@@ -0,0 +1,13 @@
+import { Dimension } from "styled-components";
+
+const dimension: Dimension = {
+  x1: "0.0625em",
+  x2: "0.125em",
+  x4: "0.25em",
+  x8: "0.5em",
+  x16: "1em",
+  x32: "2em",
+  x64: "4em",
+};
+
+export default dimension;
