a8224044930d85a2ca27796e153300e3bbb4f7dc
Ade Adeola <adedotster@gmail.com>
2020-09-27 04:12:15 +0200

    update requirements

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,15 +8,15 @@ interface BaseButtonProps {
   /**
    * Different colour codes to denote the importance or use of the button.
    */
-  variant: "primary" | "secondary" | "error" | "info" | "warning" | "success";
+  variant?: "primary" | "secondary" | "error" | "info" | "warning" | "success";
   /**
    * The size of the button
    */
-  size: "small" | "medium" | "large";
+  size?: "small" | "medium" | "large";
   /**
    * Shadows, determining how raised the button should appear. 0 means no elevation, maximum is 10
    */
-  elevation: number;
+  elevation?: number;
 }
 
 export interface ButtonProps extends BaseButtonProps {
