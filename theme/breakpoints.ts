42e0e9b826d602ec98e81dae8e6f26a16b014ab6
Ade Adeola <adedotster@gmail.com>
2020-09-27 04:58:37 +0200

    Add breakpoints

---
+++ theme/breakpoints.ts
@@ -0,0 +1,9 @@
+import { Breakpoints } from "styled-components";
+
+const breakpoints: Breakpoints = {
+  xs: "600px",
+  sm: "960px",
+  md: "1280px",
+  lg: "1920px",
+  //xl: greater than 1920px
+};
