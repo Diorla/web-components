ff11f0bf3088ee9bc2a57f5532eade9c5da8bdab
Ade Adeola <adedotster@gmail.com>
2020-11-10 11:19:29 +0100

    initial image modal

---
+++ stories/Images.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import Images from "../components/Images";
+
+export default {
+  title: "Components/Images",
+  component: Images,
+} as Meta;
+
+
+export const main = () => <Images />;
