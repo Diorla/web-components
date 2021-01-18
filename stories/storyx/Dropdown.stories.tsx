c8ff76c7b3ac10fb3173e6ffcebef51d3e7fa77b
Ade Adeola <adedotster@gmail.com>
2021-01-18 10:00:08 +0100

    create dropdown

---
+++ stories/storyx/Dropdown.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+
+import Dropdown from "../../storyx/Dropdown";
+import Link from "../../storyx/Link";
+
+export default {
+  title: "Storyx/Dropdown",
+  component: Dropdown,
+} as Meta;
+
+export const StyledDropdown = () => (
+  <div style={{ fontSize: "1.6rem" }}>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div>Hello</div>
+      <div>Hello</div>
+      <Dropdown main={<Link>Dropdown</Link>} right="12.5rem">
+        <Link href="./path/to/site">One</Link>
+        <Link href="./path/to/site">Two</Link>
+        <Link href="./path/to/site">Three</Link>
+        <Link href="./path/to/site">Four</Link>
+      </Dropdown>
+    </div>
+    <div>This is the content</div>
+  </div>
+);
---
+++ storyx/Dropdown.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+
+const slideDown = keyframes`
+  from {
+    transform: scaleY(0);
+  }
+
+  to {
+    transform: scaleY(1);
+  }
+`;
+
+const slideUp = keyframes`
+  from {
+    transform: scaleY(1);
+  }
+
+  to {
+    transform: scaleY(0);
+  }
+`;
+
+const StyledDropDown = styled.div<{ right: string; left: string }>`
+  font-size: 1.6rem;
+  font-weight: bold;
+  position: relative;
+  right: ${({ right }) => (right ? right : "initial")};
+  left: ${({ left }) => (left ? left : "initial")};
+`;
+
+const DropdownMain = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+const DropdownList = styled.div<{ expanded: boolean }>`
+  position: fixed;
+  tranform-origin: top;
+  animation: ${({ expanded }) => (expanded ? slideDown : slideUp)}
+    ${({ theme }) => theme.duration.standard} linear forwards;
+  display: flex;
+  flex-direction: column;
+  border: none;
+  box-shadow: 0 0 0.2rem;
+  padding: 0.8rem;
+  font-weight: initial;
+  min-width: 20rem;
+  transform-origin: top;
+  & > *:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+export interface DropdownProps {
+  main: React.ReactNode;
+  children: React.ReactNode;
+  right?: string;
+  left?: string;
+}
+
+const DropdownIcon = ({ expanded }: { expanded: boolean }) =>
+  expanded ? <FaCaretUp /> : <FaCaretDown />;
+
+const Dropdown = ({ main, children, left = "", right = "" }: DropdownProps) => {
+  const [loaded, setLoaded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <StyledDropDown right={right} left={left}>
+      <DropdownMain
+        onClick={() => {
+          setExpanded(!expanded);
+          setLoaded(true);
+        }}
+      >
+        {main}
+        <DropdownIcon expanded={expanded} />
+      </DropdownMain>
+      {loaded && (
+        <DropdownList
+          expanded={expanded}
+          onClick={() => {
+            setExpanded(!expanded);
+          }}
+        >
+          {children}
+        </DropdownList>
+      )}
+    </StyledDropDown>
+  );
+};
+
+export default Dropdown;
