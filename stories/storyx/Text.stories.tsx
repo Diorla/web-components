a6f20fd6b18b90e3b0c8927cecf4c3c0c5d2319e
Ade Adeola <adedotster@gmail.com>
2021-01-20 19:04:45 +0100

    create importance

--- stories/storyx/Text.stories.tsx
+++ stories/storyx/Text.stories.tsx
@@ -15,52 +15,68 @@ Header1.args = {
   children: "Header 1",
   variant: "h1",
 };
+
 export const Header2 = Template.bind({});
 Header2.args = {
   children: "Header 2",
   variant: "h2",
 };
+
 export const Header3 = Template.bind({});
 Header3.args = {
   children: "Header 3",
   variant: "h3",
 };
+
 export const Header4 = Template.bind({});
 Header4.args = {
   children: "Header 4",
   variant: "h4",
 };
+
 export const Subtitle = Template.bind({});
 Subtitle.args = {
   children: "Subtitle",
   variant: "subtitle",
 };
+
 export const Subtext = Template.bind({});
 Subtext.args = {
   children: "Subtext",
   variant: "subtext",
 };
+
 export const Caption = Template.bind({});
 Caption.args = {
   children: "Caption",
   variant: "caption",
 };
+
 export const Primary = Template.bind({});
 Primary.args = {
   children: "Primary",
   variant: "primary",
 };
+
 export const Secondary = Template.bind({});
 Secondary.args = {
   children: "Secondary",
   variant: "secondary",
 };
+
 export const Tertiary = Template.bind({});
 Tertiary.args = {
   children: "Tertiary",
   variant: "tertiary",
 };
+
 export const StyledText = Template.bind({});
 StyledText.args = {
   children: "Default Text",
+};
+
+export const Important = Template.bind({});
+Important.args = {
+  children: "Default Primary Secondary Tertiary",
+  important: true,
 };
--- storyx/Text.tsx
+++ storyx/Text.tsx
@@ -15,6 +15,7 @@ type variants =
 
 export interface TextProps {
   variant?: variants;
+  important?: boolean;
   children: React.ReactNode;
   [key: string]: any;
 }
@@ -45,23 +46,31 @@ export const Subtext = styled.span`
 export const Caption = styled.figcaption`
   font-size: 1.4rem;
 `;
-export const Primary = styled.div`
+export const Primary = styled.div<{ important: boolean }>`
   font-size: 1.6rem;
   color: ${({ theme }) => theme.primary.color};
+  text-transform: ${({ important }) => (important ? "uppercase" : "initial")};
+  font-weight: ${({ important }) => (important ? 600 : "initial")};
 `;
-export const Secondary = styled.div`
+export const Secondary = styled.div<{ important: boolean }>`
   font-size: 1.6rem;
   color: ${({ theme }) => theme.secondary.color};
+  text-transform: ${({ important }) => (important ? "uppercase" : "initial")};
+  font-weight: ${({ important }) => (important ? 600 : "initial")};
 `;
-export const Tertiary = styled.div`
+export const Tertiary = styled.div<{ important: boolean }>`
   font-size: 1.6rem;
   color: ${({ theme }) => theme.tertiary.color};
+  text-transform: ${({ important }) => (important ? "uppercase" : "initial")};
+  font-weight: ${({ important }) => (important ? 600 : "initial")};
 `;
-export const StyledText = styled.div`
+export const StyledText = styled.div<{ important: boolean }>`
   font-size: 1.6rem;
+  text-transform: ${({ important }) => (important ? "uppercase" : "initial")};
+  font-weight: ${({ important }) => (important ? 600 : "initial")};
 `;
 
-const Text: React.FC<TextProps> = ({ variant, children, props }) => {
+const Text: React.FC<TextProps> = ({ variant, children, important, props }) => {
   if (variant === "h1") return <H1 {...props}>{children}</H1>;
   else if (variant === "h2") return <H2 {...props}>{children}</H2>;
   else if (variant === "h3") return <H3 {...props}>{children}</H3>;
@@ -73,12 +82,29 @@ const Text: React.FC<TextProps> = ({ variant, children, props }) => {
   else if (variant === "caption")
     return <Caption {...props}>{children}</Caption>;
   else if (variant === "primary")
-    return <Primary {...props}>{children}</Primary>;
+    return (
+      <Primary {...props} important={important}>
+        {children}
+      </Primary>
+    );
   else if (variant === "secondary")
-    return <Secondary {...props}>{children}</Secondary>;
+    return (
+      <Secondary {...props} important={important}>
+        {children}
+      </Secondary>
+    );
   else if (variant === "tertiary")
-    return <Tertiary {...props}>{children}</Tertiary>;
-  else return <StyledText {...props}>{children}</StyledText>;
+    return (
+      <Tertiary {...props} important={important}>
+        {children}
+      </Tertiary>
+    );
+  else
+    return (
+      <StyledText {...props} important={important}>
+        {children}
+      </StyledText>
+    );
 };
 
 export default Text;
